42feb7304864a6dff9309368bef1a0def43c376f
Daniel Fullarton <linead@gmail.com>
2015-11-03 16:16:23 +1100

    Fix broken paging in meetup api

--- src/main/java/io/github/linead/nametags/MeetupData.java
+++ src/main/java/io/github/linead/nametags/MeetupData.java
@@ -23,9 +23,9 @@ public class MeetupData {
 
     private static final String EVENT_URL = "https://api.meetup.com/self/events?scroll=next_upcoming&photo-host=public&page=20&sig_id=29034422&sig=c9d17fea93c5788258d2703a5a5d92f651f65374";
 
-    private static final String RSVP_URL = "https://api.meetup.com/2/rsvps?&sign=true&key={key}&event_id={event_id}&photo-host=public&page=100";
+    private static final String RSVP_URL = "https://api.meetup.com/2/rsvps?&sign=true&key={key}&event_id={event_id}&photo-host=public&page=120";
 
-    private static final String MEMBERS_URL = "https://api.meetup.com/2/members?&key={key}&sign=true&photo-host=public&group_id={group_id}&page=100&only=id,joined,photo";
+    private static final String MEMBERS_URL = "https://api.meetup.com/2/members?&key={key}&sign=true&photo-host=public&group_id={group_id}&page=100&only=id,joined,photo&offset={page}";
 
     
     public String getKey() { return env.getProperty("meetup_api.key"); }
@@ -51,12 +51,14 @@ public class MeetupData {
         return rsvps;
     }
 
-    @Cacheable("members")
+//    @Cacheable("members")
     public Map<String, Members.Member> getMembers(String groupId) {
         RestTemplate restTemplate = new RestTemplate();
         Map<String, Object> params = new HashMap<String, Object>();
+        int page = 0;
         params.put("group_id", groupId);
         params.put("key", getKey());
+        params.put("page", page);
 
         ResponseEntity<Members> responseEntity = restTemplate.getForEntity(MEMBERS_URL, Members.class, params);
         Members members = responseEntity.getBody();
@@ -71,8 +73,9 @@ public class MeetupData {
 
         //read all pages
         while(!StringUtils.isEmpty(members.getMeta().getNext())) {
-            String nextUrl = members.getMeta().getNext() + "&key=" + getKey();
-            responseEntity = restTemplate.getForEntity(nextUrl, Members.class, params);
+            String nextUrl = members.getMeta().getNext();
+            params.put("page", ++page);
+            responseEntity = restTemplate.getForEntity(MEMBERS_URL, Members.class, params);
             members = responseEntity.getBody();
 
             memberMap.putAll(Arrays.asList(members.getResults())
